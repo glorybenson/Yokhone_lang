46a6798fb23c01b0fbab5d676b8d32e23327d462
Glory Benson <glorybenson3738@gmail.com>
2022-05-28 07:24:50 +0100

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row align-items-center">
             <div class="col-md-12">
                 <div class="d-flex align-items-center">
-                    <h5 class="page-title">{{ trans('dashboard')}}</h5>
+                    <h5 class="page-title">{{ __('dashboard')}}</h5>
                     <ul class="breadcrumb ml-2">
                         <li class="breadcrumb-item active">Users</li>
                     </ul>

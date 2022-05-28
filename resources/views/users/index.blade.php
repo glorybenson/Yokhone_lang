0c44feb089ff6656f38ab436789d012e85c073f7
Glory Benson <glorybenson3738@gmail.com>
2022-05-28 09:39:47 +0100

    joo

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row align-items-center">
             <div class="col-md-12">
                 <div class="d-flex align-items-center">
-                    <h5 class="page-title">{{ trans('dashboard')}}</h5>
+                    <h5 class="page-title">{{ trans('greetings.dashboard')}}</h5>
                     <ul class="breadcrumb ml-2">
                         <li class="breadcrumb-item active">{{ trans('greetings.users')}}</li>
                     </ul>

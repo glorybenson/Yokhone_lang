6323af87e7c84fe262cedad18bfd6bf7711f2abf
Glory Benson <glorybenson3738@gmail.com>
2022-05-28 07:58:59 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Route::get('/', function () {
-    Route::get('/{locale?}', function ($locale = null) {
+    Route::get('/', function ($locale = null) {
         if (isset($locale) && in_array($locale, config('app.available_locales'))) {
             app()->setLocale($locale);
         }
